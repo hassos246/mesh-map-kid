4044557e63dd7c40d21d07680ce38ec032d2f8f5
hassos246 <143274648+hassos246@users.noreply.github.com>
2026-01-11 00:49:34 +0100

    Change map zoom level from 10 to 8

--- content/wardrive.js
+++ content/wardrive.js
@@ -56,7 +56,7 @@ const map = L.map('map', {
   tap: false,
   zoomControl: false,
   doubleClickZoom: false
-}).setView(centerPos, 10);
+}).setView(centerPos, 8);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 15,
