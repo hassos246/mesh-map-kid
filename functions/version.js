8bc0bd8c4d97d5ce038a3e21b6920d3091267a7f
kallanreed <3761006+kallanreed@users.noreply.github.com>
2026-01-23 08:57:06 -0800

    Force version update

--- functions/version.js
+++ functions/version.js
@@ -1,4 +1,4 @@
 export async function onRequest(context) {
   // Update with to Date.now() to force reload.
-  return Response.json({ latest: 1769098745211 });
+  return Response.json({ latest: 1769187397112 });
 }
